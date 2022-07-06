2749cbe766b813199b49ba872addd3f480e0dd23
javierzor <javier20450@gmail.com>
2022-07-06 11:30:29 -0500

    Editar equipo al 70% falta campos del formulario y guardar

--- src/app/modals/editarequipo/editarequipo.page.ts
+++ src/app/modals/editarequipo/editarequipo.page.ts
@@ -10,15 +10,24 @@ import * as CryptoJS from 'crypto-js';
   styleUrls: ['./editarequipo.page.scss'],
 })
 export class EditarequipoPage implements OnInit {
-
+  respuestabeoboxtraerusuariosadministradores: any;
+  informacion_perfil: any;
+  secretKey = "123456&Descryption";
+  step: any = '1';
+  tipo_cuenta: any;
   constructor(
     private varios: VariosService,
     private modalController: ModalController,
     private variosservicios: VariosService,
 
-  ) { }
+  ) 
+  
+  {
+
+  }
 
   ngOnInit() {
+    this.obteneradministracion();    
   }
 
   dismissyactualiza() {
@@ -37,6 +46,31 @@ export class EditarequipoPage implements OnInit {
     });
   }
 
+  obteneradministracion(){
+    this.informacion_perfil=localStorage.getItem('profileInfo');
+    this.informacion_perfil=this.decrypt(this.informacion_perfil);
+    this.informacion_perfil=JSON.parse(this.informacion_perfil);
+
+    var databeoboxtraerusuariosadministradores = {
+      tipo_cuenta:this.informacion_perfil.tipo_cuenta,
+      nombre_solicitud: 'beoboxtraerusuariosadministradores'
+    }
+    this.variosservicios.variasfunciones(databeoboxtraerusuariosadministradores).subscribe(async( res: any ) =>{
+      console.log('respuesta de beoboxtraerusuariosadministradores', res);
+      this.respuestabeoboxtraerusuariosadministradores=res;
+  });
+  }
+
+  decrypt(textToDecrypt : string){
+    return CryptoJS.AES.decrypt(textToDecrypt, this.secretKey.trim()).toString(CryptoJS.enc.Utf8);
+  }
+  step1(){
+    this.step='1';
+  }
+
+  step2(){
+    this.step='2';
+  }
 
 
 }
