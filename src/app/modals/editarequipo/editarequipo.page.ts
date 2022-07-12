7ac6b60e4099faeca1d912d883245b93c7f195ea
javierzor <javier20450@gmail.com>
2022-07-12 11:27:02 -0500

    se agregan usuarios y se edita mail al crear tickets

--- src/app/modals/editarequipo/editarequipo.page.ts
+++ src/app/modals/editarequipo/editarequipo.page.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { VariosService } from 'src/app/service/varios.service';
 import * as CryptoJS from 'crypto-js';
-
+import { MenuController,LoadingController } from '@ionic/angular';
+import { PaisesService } from 'src/app/service/paises.service';
 
 @Component({
   selector: 'app-editarequipo',
@@ -15,7 +16,21 @@ export class EditarequipoPage implements OnInit {
   secretKey = "123456&Descryption";
   step: any = '1';
   tipo_cuenta: any;
+  name: any;
+  lastname: any;
+  paisID: any;
+  paisnombre: any;
+  email: any;
+  password: any;
+  referidor: any;
+  countrySelected:any;
+  countryData: any;
+  respuestabeoboxactualizarticketsmails: any;
+  paisdataseleccionado: any;
+
   constructor(
+    private paises: PaisesService,
+    private loadingController: LoadingController,
     private varios: VariosService,
     private modalController: ModalController,
     private variosservicios: VariosService,
@@ -23,6 +38,7 @@ export class EditarequipoPage implements OnInit {
   ) 
   
   {
+     this.countryData=this.paises.countryData;    
 
   }
 
@@ -70,6 +86,73 @@ export class EditarequipoPage implements OnInit {
 
   step2(){
     this.step='2';
+  }
+
+  async cambiotickets(cadaadminticketsmail, cadaadminid){
+
+    const actualizando = await this.loadingController.create({
+      message: 'Actualizando...',spinner: 'bubbles',duration: 15000,
+      });
+      actualizando.present();
+
+    if(cadaadminticketsmail){
+      console.log('desactivando');
+      var databeoboxactualizarticketsmails = {
+        id:cadaadminid,
+        nombre_solicitud: 'beoboxactualizarticketsmails1'
+      }
+      this.variosservicios.variasfunciones(databeoboxactualizarticketsmails).subscribe(async( res: any ) =>{
+        console.log('respuesta de beoboxactualizarticketsmails', res);
+        actualizando.dismiss();
+        this.obteneradministracion();
+
+    });
+
+    }
+    else{
+      console.log('activando');
+      var databeoboxactualizarticketsmails2 = {
+        id:cadaadminid,
+        nombre_solicitud: 'beoboxactualizarticketsmails2'
+      }
+      this.variosservicios.variasfunciones(databeoboxactualizarticketsmails2).subscribe(async( res: any ) =>{
+        console.log('respuesta de beoboxactualizarticketsmails2', res);
+        actualizando.dismiss();
+        this.obteneradministracion();
+
+    });
+    }
+  }
+
+  ONCHANGEPAIS(event){
+    console.log('index', event.target.value);
+    this.paisdataseleccionado= this.countryData[event.target.value];
+    console.log('paisData',this.paisdataseleccionado);
+  }
+
+
+  agregaradmin(){
+    this.variosservicios.loading2segundos("Creando espere...");
+    console.log('Agregando admin o moderador');
+    var databeoboxactualizarticketsmails = {
+      nombre_solicitud: 'beoboxcreateuser',
+      create_date: new Date(),
+      username:this.email,
+      password:this.password,
+      tipo_cuenta:this.tipo_cuenta,
+      email:this.email,
+      name:this.name,
+      lastname:this.lastname,
+      paisnombre:this.paisdataseleccionado.name,
+      paisID:this.paisdataseleccionado.id,
+      referidor: this.referidor
+    }
+    this.variosservicios.variasfunciones(databeoboxactualizarticketsmails).subscribe(async( res: any ) =>{
+      console.log('respuesta de beoboxcreateuser', res);
+      this.step='1';
+      this.variosservicios.presentToast("..::Creacion de cuenta especial exitoso::..");
+      this.obteneradministracion();
+  });
   }
 
 
